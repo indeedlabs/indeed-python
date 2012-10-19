211df61da568ebc9e1bb2e9849270344d4b66743
Chad Masso <chad.m.masso@gmail.com>
2012-10-19 12:11:13 -0500

    making version inline in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = 'indeed',
-    version = open("./VERSION").read(),
+    version = '0.0.3',
     description = 'Indeed Job Search Python Api Client',
     author = 'Indeed Labs',
     author_email = 'labs-team@indeed.com',
